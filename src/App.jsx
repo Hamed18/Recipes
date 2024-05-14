4aec580fbd15e068f3cdc3a2d9dbea2faf3d42b1
hamed18 <hamedcnec@gmail.com>
2024-05-14 12:18:19 +0600

    Successfully created a Header Component and it's working

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './Components/Header/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-    <div className="flex flex-row justify-between">
-    <h1 className='text-6xl bg-red-300'>Recipes</h1>
-     <h1>Search</h1>
-
-    </div>
+      <Header></Header>
     </>
   )
 }
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+
+const Header = () => {
+	return (
+		<div className="flex flex-row justify-between">
+            <h1 className='text-6xl bg-red-300'>Recipes</h1>
+            <h1>Search</h1>
+        </div>
+	);
+};
+
+export default Header;
