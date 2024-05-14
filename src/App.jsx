1ca460214ea95643328d65bc64be0aa8448a77c5
hamed18 <hamedcnec@gmail.com>
2024-05-14 12:58:15 +0600

    Created a json file in public-recipes.jon dirctory. fake data using gpt, imgbb. created recipes component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from './Components/Header/Header'
+import recipes from './Components/recipes/recipes'
 
 function App() {
 
   return (
     <>
       <Header></Header>
+      <recipes></recipes>
     </>
   )
 }
---
+++ src/Components/recipes/recipes.jsx
@@ -0,0 +1,10 @@
+
+const recipes = () => {
+	return (
+		<div>
+			
+		</div>
+	);
+};
+
+export default recipes;
